f29e5be4ff3e52921758d5cc7239cee79f7fe735
gnnhakan <haakangonen@gmail.com>
2017-04-22 07:05:07 -0400

    All in One

---
+++ Abc/Abc.WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Abc.WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting =
+                            Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
+            //only Return Json 
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Abc/Abc.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Abc.DAL.Entities;
+
+namespace Abc.WebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        //Create
+        public HttpResponseMessage Post([FromBody] Product product)
+        {
+            try
+            {
+                using (xyzEntities entities = new xyzEntities())
+                {
+                    entities.Products.Add(product);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, product);
+                    message.Headers.Location = new Uri(Request.RequestUri +
+                        product.ProductID.ToString());
+
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        //Update
+        public HttpResponseMessage Put(int id, [FromBody]Product product)
+        {
+            try
+            {
+                using (xyzEntities entities = new xyzEntities())
+                {
+                    var entity = entities.Products.FirstOrDefault(e => e.ProductID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                            "Product with Id " + id.ToString() + " not found to update");
+                    }
+                    else
+                    {
+                        entity.ProductName = product.ProductName;
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        //Read 
+        [HttpGet]
+        public IEnumerable<Product> LoadProducts()
+        {
+            using (xyzEntities entities = new xyzEntities())
+            {
+                entities.Configuration.ProxyCreationEnabled = false;
+                return entities.Products.ToList();
+            }
+        }
+        //get by id 
+        public HttpResponseMessage LoadProductById(int id)
+        {
+            using (xyzEntities entities = new xyzEntities())
+            {
+                var entity = entities.Products.FirstOrDefault(e => e.ProductID == id);
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                        "Product ID " + id.ToString() + " not found");
+                }
+            }
+        }
+        //Delete
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (xyzEntities entities = new xyzEntities())
+                {
+                    var entity = entities.Products.FirstOrDefault(e => e.ProductID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                            "Product ID = " + id.ToString() + " not found to delete");
+                    }
+                    else
+                    {
+                        entities.Products.Remove(entity);
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+    }
+}
