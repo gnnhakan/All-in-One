2f69faebb03a8498d35374a5c0993e3421ef778c
gnnhakan <haakangonen@gmail.com>
2017-04-22 07:24:02 -0400

    Category Controller was created.

---
+++ Abc/Abc.WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Abc.DAL.Entities;
+
+namespace Abc.WebAPI.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        private xyzEntities entities = new xyzEntities();
+        [HttpGet]
+        public IEnumerable<Category> LoadCategories()
+        {
+            entities.Configuration.ProxyCreationEnabled = false;
+            return entities.Categories.ToList();
+        }
+    }
+}
---
+++ Abc/Abc.WebFormUI/Js/app.js
@@ -0,0 +1,30 @@
+﻿var myapp = angular.module('app', [])
+.constant("ProductsUrl", "http://localhost:50009/mvcHome/Index2") // restapi url
+.constant("CategoriesUrl", "http://localhost:49268/api/categories/")
+myapp.controller("ProductController", function ($scope, $http, $ProductsUrl) {
+    $scope.getProducts = function () {
+        $scope.data = {};
+        $http.get(ProductsUrl)
+         .then(function (response) {
+             $scope.result = "Success";
+             $scope.data.products = response;
+         }, function (response) {
+             $scope.result = "Error";
+             $scope.data.products = response;
+         });
+    }
+});
+
+myapp.controller("CategoriesController", function ($scope, $http, CategoriesUrl) {
+    $scope.getProducts = function () {
+        $scope.data = {};
+        $http.get(CategoriesUrl).
+        success(function (data) {
+            $scope.data.categories = data;
+        }).error(function (error) {
+            $scope.data.error = error;
+        });
+    }
+    $scope.getProducts();
+
+});
